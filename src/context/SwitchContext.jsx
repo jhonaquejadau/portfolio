7161a962168b4f97cc5312ba8e77d4de94e9801f
jhonaquejadau <jhona.quejada@gmail.com>
2022-12-29 13:59:13 -0500

    DarkMode Context Updated

--- src/context/SwitchContext.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext, useState } from "react";
-
-const SwitchProvider = createContext();
-const SwitchContextProvider = () => {
-    const [darkMode, setDarkmode] = useState(false)
-    return (
-        <></>
-    )
-}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom"
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import { DarkModeProvider } from './context/DarkModeContext';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter basename='/portfolio'>
-    <App />
+      <DarkModeProvider>
+        <App />
+      </DarkModeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
